d2cd139482afe685206811739470e660469a09f9
Abin Joseph <itsmeabjs@gmail.com>
2021-10-08 18:32:25 +0530

    update

--- data.js
+++ data.js
@@ -1,45 +1,45 @@
 const data = {
     abin: {
-        fname: "abin",
-        lname: "joseph",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
+      fname: "abin",
+      lname: "joseph",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
     },
     anju: {
-        fname: "anju",
-        lname: "gopan",
-        dept: "CSE",
-        NumberOfPhotos: 2,
+      fname: "anju",
+      lname: "gopan",
+      dept: "CSE",
+      NumberOfPhotos: 2,
     },
     poojalakshmi: {
-        fname: "poojalakshmi",
-        lname: "pk",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
+      fname: "poojalakshmi",
+      lname: "pk",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
     },
-     haripriya: {
-        fname: "haripriya",
-        lname: "ks",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
-    },  
+    haripriya: {
+      fname: "haripriya",
+      lname: "ks",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
+    },
     pooja: {
-        fname:"pooja",
-        lname:"mp",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
-   },
- athira: {
-        fname:"athira",
-        lname:"menon",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
- },
-     jeethu: {
-        fname: "jeethu",
-        lname: "joshy",
-        dept: "Computer science and engineering",
-        NumberOfPhotos: 2,
-    }
-}
-export default data;
+      fname: "pooja",
+      lname: "mp",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
+    },
+    athira: {
+      fname: "athira",
+      lname: "menon",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
+    },
+    jeethu: {
+      fname: "jeethu",
+      lname: "joshy",
+      dept: "Computer science and engineering",
+      NumberOfPhotos: 2,
+    },
+  };
+  export default data;
