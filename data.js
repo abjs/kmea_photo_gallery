71872187d79360ff24da9aa98ea0df388d1c51af
Abin Joseph <itsmeabjs@gmail.com>
2021-10-10 15:11:20 +0530

    docs: add thaniyas's pics

--- data.js
+++ data.js
@@ -1,51 +1,51 @@
 const data = {
-    anju: {
-      fname: "anju",
-      lname: "gopan",
-      dept: "CSE",
-      NumberOfPhotos: 2,
-    },
-    pooj: {
-      fname: "poojalakshmi",
-      lname: "pk",
-      dept: "Computer science and engineering",
-      NumberOfPhotos: 2,
-    },
-    haripriya: {
-      fname: "haripriya",
-      lname: "ks",
-      dept: "Computer science and engineering",
-      NumberOfPhotos: 2,
-    },
-    pooja: {
-      fname: "pooja",
-      lname: "mp",
-      dept: "Computer science and engineering",
-      NumberOfPhotos: 2,
-    },
-    athira: {
-      fname: "athira",
-      lname: "menon",
-      dept: "Computer science and engineering",
-      NumberOfPhotos: 2,
-    },
-    jeethu: {
-      fname: "jeethu",
-      lname: "joshy",
-      dept: "Computer science and engineering",
-      NumberOfPhotos: 2,
-    },
-    mansoor: {
-        fname:"mansoor",
-        lname: "majeed",
-        dept: "computer science and engineering",
-        NumberOfPhotos: 2,
-    },    
-        thaniya: {
-        fname:"thaniya",
-        lname: "azhar",
-        dept: "vidyodaya",
-        NumberOfPhotos: 2,
-    }
-  };
-  export default data;
+  anju: {
+    fname: "anju",
+    lname: "gopan",
+    dept: "CSE",
+    NumberOfPhotos: 2,
+  },
+  pooj: {
+    fname: "poojalakshmi",
+    lname: "pk",
+    dept: "Computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  haripriya: {
+    fname: "haripriya",
+    lname: "ks",
+    dept: "Computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  pooja: {
+    fname: "pooja",
+    lname: "mp",
+    dept: "Computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  athira: {
+    fname: "athira",
+    lname: "menon",
+    dept: "Computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  jeethu: {
+    fname: "jeethu",
+    lname: "joshy",
+    dept: "Computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  mansoor: {
+    fname: "mansoor",
+    lname: "majeed",
+    dept: "computer science and engineering",
+    NumberOfPhotos: 2,
+  },
+  thaniya: {
+    fname: "thaniya",
+    lname: "azhar",
+    dept: "vidyodaya",
+    NumberOfPhotos: 2,
+  },
+};
+export default data;
